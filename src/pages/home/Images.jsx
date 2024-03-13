001873eb181c3648e24a56f388d008e61ebc27eb
S-YUN <candy9won@naver.com>
2024-03-13 14:32:51 +0900

    image 기능 테스트 wip

--- src/pages/home/Images.jsx
+++ src/pages/home/Images.jsx
@@ -2,33 +2,22 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { changeImgList } from '../../states/store';
 
 function Images() {
 
-    const [productImg, setProductImg] = useState([])
-    const [previewImg, setPreviewImg] = useState([])
+    const dispatch = useDispatch()
+    const [previewImages, setPreviewImages] = useState([]);
 
-
-
-    function uploadFile(e) {
-        let fileArr = e.target.files;
-        setProductImg(Array.from(fileArr));
-
-        let fileUrl = [];
-        for (let i = 0; i < fileArr.length; i++) {
-            let fileRead = new FileReader();
-            fileRead.onload = function () {
-                fileUrl[i] = fileRead.result;
-                setPreviewImg([...fileUrl]);
-                fileRead.readAsDataURL(fileArr[i]);
-            }
-        };
-    }
-    const handleUpload = () => {
-        // 여기에서 선택된 파일들을 업로드하는 로직을 추가합니다.
-        console.log(previewImg);
-        console.log(productImg);
+    // 파일 선택 시 이미지 미리보기 및 Redux에 저장
+    const handleFileChange = (e) => {
+        const files = Array.from(e.target.files);
+        const imageUrls = files.map(file => URL.createObjectURL(file));
+        setPreviewImages(imageUrls); // 이미지 미리보기 업데이트
+        dispatch(changeImgList(imageUrls)); // 이미지 배열을 Redux 상태에 저장
     };
+
 
     return (
         //컴포넌트
@@ -38,21 +27,18 @@ function Images() {
                     이미지
                 </Form.Label>
                 <Col >
-                    <input type="file" multiple onChange={uploadFile} />
-                    {/* <button onClick={handleUpload}>업로드버튼인데.. 아마 필요없을듯</button> */}
-                    {productImg.length > 0 && (
-                        <div>
-                            {productImg.map((file, index) => (
-                                <img
-                                    key={index}
-                                    src={URL.createObjectURL(file)}
-                                    alt={`Preview ${index}`}
-                                    style={{ height:'200px', padding: '10px' }}
-                                />
-                            ))}
+                    {/* 파일 입력란 */}
+                    <input type="file" multiple onChange={handleFileChange} />
 
-                        </div>
-                    )}
+                    {/* 선택된 파일들의 미리보기 */}
+                    <div>
+                        {previewImages.map((imageUrl, index) => (
+                            <img key={index} src={imageUrl}
+                                // alt부분을 이미지 파일이 아닙니다. 이런형식으로 알려주는 역할을 할수있을듯
+                                alt={`Preview ${index}`}
+                                style={{ width: '100px', height: '100px', marginRight: '10px' }} />
+                        ))}
+                    </div>
 
                 </Col>
             </Form.Group>
