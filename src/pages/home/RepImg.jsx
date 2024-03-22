5a36262a46e97b11da7b628c09f4b67b6345f1ac
S-YUN <candy9won@naver.com>
2024-03-22 16:40:30 +0900

    이미지 박스에 맞춤

--- src/pages/home/RepImg.jsx
+++ src/pages/home/RepImg.jsx
@@ -4,47 +4,58 @@ import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { changeRepImg } from '../../states/store';
+import { FaCamera } from 'react-icons/fa';
 import styled from 'styled-components'
 
 function RepImg() {
 
-    const dispatch = useDispatch()
-    const [previewImage, setPreviewImage] = useState();
+  const dispatch = useDispatch()
+  const [previewImage, setPreviewImage] = useState();
 
-    // 파일 선택 시 이미지 미리보기 및 Redux에 저장
-    const handleFileChange = (e) => {
-        const file = e.target.files[0];
-        const imageUrl = URL.createObjectURL(file ,{ quality: 1 });
-        setPreviewImage(imageUrl); // 이미지 미리보기 업데이트
-        dispatch(changeRepImg([imageUrl])); // 이미지 배열을 Redux 상태에 저장 (단일 이미지)
-    };
+  // 파일 선택 시 이미지 미리보기 및 Redux에 저장
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setPreviewImage(imageUrl); // 이미지 미리보기 업데이트
+      dispatch(changeRepImg([imageUrl])); // 이미지 배열을 Redux 상태에 저장 (단일 이미지)
+    }
+  };
+
+  // 이미지 클릭 시 새 탭에서 원본 이미지 열기
+  const handlePreviewClick = () => {
+    if (previewImage) {
+      window.open(previewImage, '_blank');
+    }
+  };
 
 
-    return (
-        //컴포넌트
-        <div >
-            <Form.Group as={Row} className="mb-3">
-                <Form.Label column sm="2">
-                    대표 이미지
-                </Form.Label>
-                <Col >
-                    <Container>
-                        {/* 이미지 미리보기 */}
-                        <ImagePreviewContainer>
-                            {previewImage && <ImagePreview src={previewImage} alt="Preview" />}
-                        </ImagePreviewContainer>
 
-                        {/* 파일 선택 */}
-                        <FileInputLabel htmlFor="fileInput">
-                            파일 선택
-                            <FileInput id="fileInput" type="file" onChange={handleFileChange} />
-                        </FileInputLabel>
-                    </Container>
+  return (
+    <div >
+      <Form.Group as={Row} className="mb-3">
+        <Form.Label column sm="2">
+          대표 이미지
+        </Form.Label>
+        <Col >
+          <Container>
 
-                </Col>
-            </Form.Group>
-        </div>
-    )
+            {/* 파일 선택 */}
+            <FileInputLabel htmlFor="fileInput">
+              <FileInputIcon /> {/* 이미지 아이콘 */}
+              이미지 선택
+              <FileInput id="fileInput" type="file" onChange={handleFileChange} />
+            </FileInputLabel>
+            {/* 이미지 미리보기 */}
+            <ImagePreviewContainer onClick={handlePreviewClick}>
+              {previewImage && <ImagePreview src={previewImage} alt="Preview" />}
+            </ImagePreviewContainer>
+          </Container>
+
+        </Col>
+      </Form.Group>
+    </div>
+  )
 
 }
 
@@ -54,19 +65,20 @@ const Container = styled.div`
 `;
 
 const ImagePreviewContainer = styled.div`
-  width: 300px;
-  height: 300px;
+  width: 200px;
+  height: 200px;
   border: 1px solid #ccc;
-  margin-right: 10px;
+  margin-right: 5px;
+  margin-left: 5px;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const ImagePreview = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+width: 100%;
+    height: 100%;
+    object-fit: cover;
 `;
 
 const FileInput = styled.input`
@@ -74,12 +86,24 @@ const FileInput = styled.input`
 `;
 
 const FileInputLabel = styled.label`
-  cursor: pointer;
-  padding: 10px 15px;
-  background-color: #007bff;
-  color: #fff;
-  border-radius: 5px;
-  text-align: center;
+    position: relative;
+    cursor: pointer;
+    padding: 10px 15px;
+    width: 200px;
+    height: 200px;
+    background-color: #F6F5F5;
+    border: 1px solid #ccc;
+    color: grey;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const FileInputIcon = styled(FaCamera)`
+    font-size: 30px;
+    margin-bottom: 10px;
 `;
 
 
