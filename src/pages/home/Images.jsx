02492c189e2353debf8ef14660d6e73cef020eba
S-YUN <https://github.com/S-YUN>
2024-03-23 12:26:53 +0900

    추가 이미지 삭제 기능. 사진 미리보기 비율 맞추기 확정

--- src/pages/home/Images.jsx
+++ src/pages/home/Images.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { changeImgList } from '../../states/store';
 import styled from 'styled-components'
-import { FaCamera } from 'react-icons/fa';
+import { FaCamera, FaTimes } from 'react-icons/fa';
 
 function Images() {
 
@@ -18,7 +18,7 @@ function Images() {
         if (files) {
             const totalImages = files.length + previewImages.length;
             if (totalImages > 9) {
-                alert("이미지는 최대 9개까지 업로드할 수 있습니다.");
+                alert("추가 이미지는 최대 9개까지 업로드할 수 있습니다.");
             }
             const imageUrls = Array.from(files).map(file => URL.createObjectURL(file));
             const newPreviewImages = [...previewImages, ...imageUrls].slice(0, 9); // 최대 9개까지만 추가
@@ -34,71 +34,75 @@ function Images() {
         }
     };
 
+    // 이미지 삭제 버튼 클릭 시
+    const handleImageDelete = (index) => {
+        const newPreviewImages = [...previewImages];
+        newPreviewImages.splice(index, 1); // 선택된 이미지 삭제
+        setPreviewImages(newPreviewImages);
+        dispatch(changeImgList(newPreviewImages)); // Redux에 변경된 이미지 목록 저장
+    };
 
     return (
-        //컴포넌트
-        <div >
+        <div>
             <Form.Group as={Row} className="mb-3">
                 <Form.Label column sm="2">
                     추가 이미지
                 </Form.Label>
-                <Col >
+                <Col>
                     <Container>
                         <ImageGrid>
-                            {/* 파일 선택 */}
                             <FileInputLabel htmlFor="multiFileInput">
-                                <FileInputIcon /> {/* 이미지 아이콘 */}
+                                <FileInputIcon />
                                 이미지 선택
                                 <FileInput id="multiFileInput" type="file" multiple onChange={handleFileChange} />
                             </FileInputLabel>
-                            {/* 선택된 파일들의 미리보기 */}
                             {previewImages.map((imageUrl, index) => (
                                 <ImagePreviewContainer key={index}>
+                                    <DeleteButton onClick={() => handleImageDelete(index)}>
+                                        <FaTimes />
+                                    </DeleteButton>
                                     <ImagePreview src={imageUrl} alt="이미지 파일이 아닙니다" onClick={() => handlePreviewClick(imageUrl)} />
                                 </ImagePreviewContainer>
                             ))}
                         </ImageGrid>
                     </Container>
                     <TextLabel>* 미리보기 이미지 클릭시 원본 이미지 확인가능</TextLabel>
-
                 </Col>
-
             </Form.Group>
         </div>
     )
-
 }
 
 const Container = styled.div`
-  display: flex;
+    display: flex;
     align-items: flex-start; /* 이미지 선택 버튼과 이미지 그리드를 위쪽으로 정렬 */
 `;
 
 const ImageGrid = styled.div`
-     display: grid;
+    display: grid;
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
     grid-gap: 10px;
     width: 100%;
 `;
 
 const ImagePreviewContainer = styled.div`
+    position: relative; 
     width: 200px;
     height: 200px;
     border: 1px solid #ccc;
     display: flex;
     justify-content: center;
     align-items: center;
-    overflow: hidden; /* 이미지가 컨테이너를 벗어날 경우를 대비하여 오버플로우를 숨김 */
+    overflow: hidden;
 `;
 
 const ImagePreview = styled.img`
-   width: 100%; /* 이미지가 컨테이너에 꽉 차도록 설정 */
-    height: 100%; /* 이미지가 컨테이너에 꽉 차도록 설정 */
-    object-fit: cover; /* 이미지가 확대되거나 축소되어 컨테이너에 맞도록 설정 */
+    max-width: 100%;
+    max-height: 100%;
 `;
 
 const FileInput = styled.input`
-  display: none;
+    display: none;
 `;
 
 const FileInputLabel = styled.label`
@@ -124,6 +128,29 @@ const FileInputIcon = styled(FaCamera)`
 const TextLabel = styled.label`
     font-size: 12px;
     color: grey;
+`;
+
+const DeleteButton = styled.button`
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    z-index: 1;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: background-color 0.3s ease;
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.7);
+    }
 `;
 
 export default Images;
