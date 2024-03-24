4d64d2bb23df3ca38549b5d36be9eb71935274fc
S-YUN <candy9won@naver.com>
2024-03-25 00:04:47 +0900

    카테고리 기능/ui

--- src/pages/home/Category.jsx
+++ src/pages/home/Category.jsx
@@ -1,32 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { Col, Form, Row } from 'react-bootstrap';
 import { CATEGORIES } from './CATEGORYLIST';
-import { HiOutlineChevronRight } from 'react-icons/hi';
+import { HiOutlineChevronRight, HiOutlineChevronDown } from 'react-icons/hi';
+import { useDispatch } from 'react-redux';
+import { changeCategory } from '../../states/store';
 
 const animation = {
     mount: keyframes`
-    0% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 1;
-    }
+    0% { opacity: 1;}
+    100% {opacity: 1;}
   `,
     unmount: keyframes`
-    0% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 1;
-    }
+    0% {opacity: 1;}
+    100% {opacity: 1;}
   `,
 };
 
 const Category = () => {
-    const [currentCity, setCurrentCity] = useState(1);
+    const dispatch = useDispatch();
+
+    const [mainCategory, setMainCategory] = useState(1);
     const [categoryAnimation, setCategoryAnimation] = useState(true);
     const [showCategory, setShowCategory] = useState(false);
+
+    const [selectedSubCateName, setSelectedSubCateName] = useState(null);
+    const categoryRef = useRef(null);
+    const categoryButtonRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                categoryRef.current &&
+                !categoryRef.current.contains(event.target) &&
+                categoryButtonRef.current &&
+                !categoryButtonRef.current.contains(event.target)
+            ) {
+                setShowCategory(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     const closeCategory = () => {
         setCategoryAnimation(false);
@@ -36,99 +54,58 @@ const Category = () => {
         setShowCategory(!showCategory);
     };
 
-    const moveToCategory = id => {
-        setCurrentCity(id);
+    const selectSubCategory = (id, name) => {
+        dispatch(changeCategory(id));
+        setSelectedSubCateName(name);
         setShowCategory(false);
     };
 
-    const S = {
-        CategoryBox: styled.div`
-      ${flex()}
-      position: absolute;
-      top: calc(100% + 10px); /* 카테고리 선택 버튼 바로 아래 */
-      left: px;
-      width: 470px;
-      height: 555px;
-      background-color: white;
-      border-radius: 5px;
-      border: none;
-      box-shadow: 0 1px 8px 1px rgba(0, 0, 0, 0.3);
-      color: black;
-      animation: ${({ categoryAnimation }) =>
-                categoryAnimation ? animation.mount : animation.unmount} 0.1s;
-      z-index: 5;
-    `,
-        Cities: styled.ul`
-      width: 168px;
-      height: 100%;
-      padding: 23px 10px 0 36px;
-      border-right: 0.5px solid ${({ theme }) => theme.lightGrey};
-    `,
-        SubCategories: styled.ul`
-      width: 302px;
-      height: 100%;
-      padding: 23px 0 0 36px;
-    `,
-        Category: styled.li`
-      ${flex('space-between', 'center', 'row')};
-      width: 100%;
-      height: 27px;
-      margin-bottom: 5px;
-      color: ${({ isSelected, theme }) =>
-                isSelected ? theme.brandColor : 'black'};
-      font-size: 13px;
-      font-weight: 100;
-      cursor: pointer;
-      &:hover {
-        color: ${({ theme }) => theme.brandColor};
-      }
-    `,
-    };
-
     return (
-        <div style={{ position: 'relative' }}> {/* 부모 요소에 position: relative; 추가 */}
+        <div style={{ position: 'relative' }}>
             <Form.Group as={Row} className="mb-3">
                 <Form.Label column sm="2">
                     카테고리
                 </Form.Label>
                 <Col>
                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <CategoryButton onClick={toggleCategorySelection}>
-                            <CategoryButtonText>카테고리 선택</CategoryButtonText>
-                            <CategoryButtonIcon>▼</CategoryButtonIcon>
+                        <CategoryButton ref={categoryButtonRef} onClick={toggleCategorySelection}>
+                            {selectedSubCateName || '카테고리 선택'}
+                            <span>&nbsp;</span>
+                            <HiOutlineChevronDown />
                         </CategoryButton>
                     </div>
                     {showCategory && (
                         <S.CategoryBox
                             onMouseLeave={closeCategory}
                             categoryAnimation={categoryAnimation}
+                            ref={categoryRef}
                         >
-                            <S.Cities>
-                                {CATEGORIES.map(city => (
+                            <S.MainCategories>
+                                {CATEGORIES.map((first_cate) => (
                                     <S.Category
-                                        key={city.id}
-                                        onMouseEnter={() => setCurrentCity(city.id)}
-                                        isSelected={currentCity === city.id}
+                                        key={first_cate.id}
+                                        onMouseEnter={() => setMainCategory(first_cate.id)}
+                                        isSelected={mainCategory === first_cate.id}
                                     >
-                                        <span>{city.name}</span>
-                                        {currentCity === city.id && (
+                                        <span>{first_cate.name}</span>
+                                        {mainCategory === first_cate.id && (
                                             <span>
                                                 <HiOutlineChevronRight />
                                             </span>
                                         )}
                                     </S.Category>
                                 ))}
-                            </S.Cities>
+                            </S.MainCategories>
                             <S.SubCategories>
-                                {CATEGORIES.find(category => category.id === currentCity)
-                                    .subcategories.map(subCategory => (
-                                        <S.Category
-                                            key={subCategory.subCategoryId}
-                                            isSelected={parseInt(subCategory.subCategoryId) === subCategory.subCategoryId}
-                                            onClick={() => moveToCategory(subCategory.subCategoryId)}
+                                {(CATEGORIES.find((category) => category.id === mainCategory) || { subcategories: [] })
+                                    .subcategories.map((sub_cate) => (
+                                        <S.SubCategory
+                                            key={sub_cate.subCategoryId}
+                                            isSelected={selectedSubCateName === sub_cate.name}
+                                            onClick={() => selectSubCategory(sub_cate.subCategoryId, sub_cate.name)}
                                         >
-                                            {subCategory.name}
-                                        </S.Category>
+                                            {sub_cate.name}
+                                        </S.SubCategory>
                                     ))}
                             </S.SubCategories>
                         </S.CategoryBox>
@@ -147,22 +124,85 @@ const flex = (justify = 'center', align = 'center', direction = 'row') => `
 `;
 
 const CategoryButton = styled.button`
-  display: flex;
-  align-items: center;
-  padding: 10px;
-  border: 1px solid #DEE2E6;
-  border-radius: 10px;
-  background-color: white;
-  cursor: pointer;
+    display: flex;
+    align-items: center;
+    min-height: 50px;
+    padding: 5px 20px;
+    border: 1px solid #dee2e6;
+    font-weight: bold;
+    border-radius: 10px;
+    background-color: white;
+    cursor: pointer;
+     /* 내부 요소들 사이에 마진 추가 */
+     span {
+        margin-right: 10px; /* 원하는 만큼의 여백 조절 가능 */
+    }
 `;
 
-const CategoryButtonText = styled.span`
-  margin-right: 5px;
-`;
 
-const CategoryButtonIcon = styled.span`
-  font-size: 1.2em;
-`;
+const S = {
+    CategoryBox: styled.div`
+        ${flex()}
+        position: absolute;
+        top: calc(100% + 10px); /* 카테고리 선택 버튼 바로 아래 */
+        width: 650px;
+        height: 555px;
+        padding: 10px;
+        background-color: white;
+        border-radius: 5px;
+        border: none;
+        box-shadow: 0 1px 8px 1px rgba(0, 0, 0, 0.3);
+        color: black;
+        animation: ${({ categoryAnimation }) =>
+            categoryAnimation ? animation.mount : animation.unmount} 0.1s;
+        z-index: 5;
+    `,
+    MainCategories: styled.ul`
+        width: 150px;
+        height: 100%;
+        padding: 20px 10px 0 20px;
+        /* border: 1px solid black; */
+        /* border-right: 0.5px solid lightgray; */
+    `,
+    SubCategories: styled.ul`
+        width: 500px;
+        height: 100%;
+        padding: 20px 10px 0 20px;
+        display:flex;
+        align-content:flex-start;
+        flex-direction:column;
+        flex-wrap:wrap;
+        overflow:auto;
 
+    `,
+    Category: styled.li`
+        ${flex('space-between', 'center', 'row')};
+        width: 100%;
+        height: 35px;
+        margin-bottom: 5px;
+        color: ${({ isSelected }) =>
+            isSelected ? 'blue' : 'black'};
+        font-size: 15px;
+        font-weight: 500;
+        cursor: pointer;
+        &:hover {
+            color: ${({ isSelected }) => (isSelected ? 'blue' : 'black')}; /* 선택된 카테고리는 파란색, 그 외에는 회색 */
+        }
+    `,
+    SubCategory: styled.li`
+    ${flex('space-between', 'center', 'row')};
+    width: 250px;
+    height: 35px;
+    margin-bottom: 7px;
+    color: ${({ isSelected }) =>
+            isSelected ? 'blue' : 'black'};
+    font-size: 15px;
+    font-weight: 200;
+    cursor: pointer;
+    &:hover {
+        color: blue
+    }
+`,
+};
 
 export default Category;
