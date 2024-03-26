85c718e05582be3982120235bd5654819da5b9e9
S-YUN <candy9won@naver.com>
2024-03-27 04:00:06 +0900

    구글 번역 비활성화 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
-
-import { Routes, Route, Link } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home';
-import { Button, Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 
 function App() {
+
+  // 크롬에서 구글 번역 응용 프로그램이 활성화되어 있는지 확인하는 함수
+  function isTranslationAppEnabled() {
+    return window.chrome && window.chrome.translate && window.chrome.translate.onTranslateEnabled();
+  }
+
+  // 구글 번역을 비활성화하는 함수
+  function disableTranslation() {
+    if (isTranslationAppEnabled()) {
+      document.documentElement.classList.add('notranslate');
+      window.chrome.translate.setTranslateEnabled(false);
+    }
+  }
+  // 앱이 로드될 때 번역을 비활성화
+  useEffect(() => {
+    disableTranslation();
+  }, [])
+  
   return (
     <div className="App">
-
-<Navbar className="bg-body-tertiary">
+      <Navbar className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="/">
             <img
